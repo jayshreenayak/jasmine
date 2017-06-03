8ac7af7b65bf3dfcb43c1513f3b1caa5e9e9e700
jasmine <jayshreenayak550@gmail.com>
2017-06-03 07:46:38 +0530

    initial submission to rep

---
+++ JayshreeFramework28thMay17Latest/Base.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JayshreeFramework28thMay17Latest
+{
+    [TestFixture]
+    public class Base
+    {
+        public StreamWriter sw;
+       [OneTimeSetUp]
+        public void BeforeTestScenario()
+        {
+            sw = new StreamWriter(@"E:\VisualStudioProject\JayshreeFramework28thMay17Latest\OutPut.Txt");
+            sw.WriteLine("Before test scenario");
+            
+        }
+        [TearDown]
+        public void AfterTestMethod()
+        {
+            Console.WriteLine("After testmethod");
+            sw.WriteLine("After test method");
+        }
+        [OneTimeTearDown]
+        public void AfterTestScenario()
+        {
+            Console.WriteLine("After testscenario");
+            sw.WriteLine("After test scenario");
+            sw.Close();
+        }
+       
+
+
+
+
+    }
+}
---
+++ JayshreeFramework28thMay17Latest/TestClassSet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace JayshreeFramework28thMay17Latest
+{
+    [TestFixture]
+
+    class TestClassSet:Base
+    {
+        [Test]
+        public void TestCase1()
+        {
+            Console.WriteLine("Test case1 execution");
+            sw.WriteLine("Tesr case1 execution");
+        }
+
+        [Test]
+
+        public void TestCase2()
+        {
+            Console.WriteLine("Test case2 execution");
+            sw.WriteLine("Tesr case2 execution");
+        }
+
+    }
+}
